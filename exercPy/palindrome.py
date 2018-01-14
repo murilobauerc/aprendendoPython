4f94a95a71254f9fded295d6a0e248cd38221b5d
Murilo <panchobauer@gmail.com>
2018-01-14 20:09:28 -0200

    Adicionando comentarios

--- exercPy/palindrome.py
+++ exercPy/palindrome.py
@@ -4,4 +4,7 @@ palavra = input('Informe uma palavra e veja se ela é palíndrome: ')
 if palavra == palavra[::-1]:
     print('%s é palíndrome.' %palavra)
 else:
-    print('A palavra não é palíndrome.')
+    print('%s não é palíndrome.' %palavra)
+
+# arara == arara É palíndrome
+# murilo != olirum Não é palíndrome
