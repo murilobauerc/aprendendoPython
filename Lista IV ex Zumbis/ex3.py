6a5cbbd8aa5486c69b9082cf91e05acd39d900b1
Murilo <panchobauer@gmail.com>
2018-01-16 23:45:58 -0200

    C:/Program Files/Git/edit/ Retornando o numero digitado invertido!

--- Lista IV ex Zumbis/ex3.py
+++ Lista IV ex Zumbis/ex3.py
@@ -1,4 +1,8 @@
 # Faça um programa que peça um inteiro positivo e o mostre invertido. Ex.: 1234 gera 4321
 num = input('Informe um número: ') # aqui a sacada é tratar o número como se fosse string, para que eu possa inverter
-num = num[::-1] # do final do número até o inicio, andando -1 casa.
-print (num)
+if num > '1':
+    num = num[::-1] # do final do número até o inicio, andando -1 casa.
+    print (num)
+else:
+    print('Voce digitou um numero negativo.')
+
