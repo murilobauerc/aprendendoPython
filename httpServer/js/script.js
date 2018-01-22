2cb96b1c62a0e86bad6bbeb1459a7783e98f630c
Murilo <panchobauer@gmail.com>
2018-01-22 15:12:07 -0200

    Aplicacao de servidor Python

---
+++ httpServer/js/script.js
@@ -0,0 +1,12 @@
+/* JQUERY SINTAXE
+ $(selector).action() */
+
+/* $('#caixas').click(function(){
+  alert(1);
+}); */
+
+document.getElementByClass("thread").addEventListener("click", myFunction);
+
+function myFunction() {
+    alert(1);
+}
