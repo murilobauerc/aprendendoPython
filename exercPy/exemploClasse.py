b8e86217e5c4ca027a14a3d0ec7c4c52dd3e9793
Murilo <panchobauer@gmail.com>
2018-01-25 20:17:47 -0200

    editing class

--- exercPy/exemploClasse.py
+++ exercPy/exemploClasse.py
@@ -1,14 +1,29 @@
-import datetime
-
 class Pessoa():
+    import datetime
+    objects = [ ]
+    qtd = 0
     def __init__(self, nome, nascimento):
         self.nome = nome
         self.nascimento = nascimento
     def idade(self):
+        import datetime
         delta = datetime.date.today() - self.nascimento
         return int(delta.days/365)
     def __str__(self):
-        return '%s, %d anos' %(self.nome, self.idade())
+        for i in self.objects:
+            print(i[0], i[1])
+    def grava(self):
+        Pessoa.qtd += 1
+        Pessoa.objects.append(['Nome:', nome])
+        Pessoa.objects.append(['Idade:', idade])
+while True:
+    nome = input('Informe um nome:')
+    idade = int(input('Informe uma idade:'))
+    obj = Pessoa(nome,idade)
+    obj.grava()
+    print('Deseja inserir mais um colaborador no sistema?\n1 - Inserir\n2 - Encerrar.')
+    condicao = int(input('Digite 1 para inserir, 2 para encerrar o programa:'))
+    if not condicao == 1:
+        print(obj.__str__())
+        break
 
-bauer = Pessoa('Murilo Bauer', datetime.date(1998, 1,30))
-print(bauer)
